f56c17b6d15bc6be620ef590724aa3490bf72a94
TamarHayat <tahayat@g.jct.ac.il>
2024-08-29 12:12:06 +0300

    adding be

---
+++ Flights_BE/User.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from FlightInfo import FlightInfoPartial, FlightInfo
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.flights = {}  # Dictionary to hold <dateTime, flightInfo>
+
+    def add_flight(self, flight_datetime, flight_info_part):
+        if isinstance(flight_datetime, datetime) and isinstance(flight_info_part, FlightInfoPartial):
+            self.flights[flight_datetime] = flight_info_part
+        else:
+            raise ValueError("Invalid types for flight_datetime or flight_info")
+
+    def __repr__(self):
+        return f"User(username={self.username}, flights={self.flights})"
