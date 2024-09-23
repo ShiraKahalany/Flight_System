43736676e90245a06f3887ea0be84797afdef886
TamarHayat <tahayat@g.jct.ac.il>
2024-09-23 12:08:59 +0300

    prediction in the server

---
+++ AppServer/AppServer/API/PredictionController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppServer.Models;
+using AppServer.Services;
+using System.Threading.Tasks;
+using AppServer.Models;
+
+namespace AppServer.API
+{
+    [Route("api/prediction")]
+    [ApiController]
+    public class PredictionController : ControllerBase
+    {
+        private readonly PredictionService _predictionService;
+
+        public PredictionController()
+        {
+            _predictionService = new PredictionService();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PredictFlightDelay([FromBody] Flight_Details flightDetails)
+        {
+            if (flightDetails == null)
+            {
+                return BadRequest("Invalid flight details.");
+            }
+
+            try
+            {
+                var prediction = await _predictionService.GetFlightDelayPrediction(flightDetails);
+
+                // Return true if there's a delay (prediction == 1), otherwise false
+                bool result = (bool)prediction;
+
+                return Ok(result); // Return boolean value directly
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, $"Error calling prediction service: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ AppServer/AppServer/Models/Flight_Details.cs
@@ -0,0 +1,22 @@
+﻿namespace AppServer.Models
+{
+    public class Flight_Details
+    {
+        public string Season { get; set; }
+        public int FlightDistance { get; set; }
+        public int FlightDuration { get; set; }
+        public int DepartureAirportCongestion { get; set; }
+        public int ArrivalAirportCongestion { get; set; }
+        public string DayOfWeek { get; set; }
+        public string TimeOfFlight { get; set; }
+        public string ScheduledDepartureTime { get; set; }
+        public string ActualDepartureTime { get; set; }
+        public int DepartureDelay { get; set; }
+        public double Temperature { get; set; }
+        public double Visibility { get; set; }
+        public double WindSpeed { get; set; }
+        public string WeatherEvent { get; set; }
+    }
+}
+
+
